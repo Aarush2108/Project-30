d87ebf434000765c7358926a8aa58ff5f3433d9a
Aarush2108 <73521015+Aarush2108@users.noreply.github.com>
2020-12-25 15:29:02 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,32 @@
+class Block{
+    constructor(x,y){
+        var options={
+            restitution:0.4
+        }
+        this.width=30;
+        this.height=40;
+        this.body=Bodies.rectangle(x,y,30,40,options);
+        World.add(world,this.body);
+        this.visiblity = 255;
+    }
+    display(){
+        var pos=this.body.position;
+        if(this.body.speed<3){
+            push();
+            translate(pos.x,pos.y);
+            rectMode(CENTER);
+            stroke("black");
+            rect(0,0,this.width,this.height);
+           pop();
+        }
+        else{
+         World.remove(world,this.body);
+         push();
+         this.visiblity = this.visiblity-5;
+        tint(255,this.visiblity);
+        pop();
+        }
+
+        
+    }
+}
